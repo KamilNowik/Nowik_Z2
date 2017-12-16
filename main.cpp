9e87a5b5e7c72ce37cce5ec77f003fb0ca38f56b
KamilNowik <34374860+KamilNowik@users.noreply.github.com>
2017-12-16 22:41:57 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Podaj ciag znakow konczacy sie kropka lub wykrzyknikiem" << endl;
+    char zn;
+    do
+    {
+        cin >> zn;
+    }
+    while (zn != '.')
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
